68cc2796c206aa81c88717c35e5db8a06d74e451
Simon Kinslow <simonkinslow@gmail.com>
2014-07-02 13:10:32 +0100

    Code Clean Up
    Removed some CSS3 conflicts with vendor prefixing, when the animation ends the punch animation is removed to help with performance

--- _assets/_js/Main.js
+++ _assets/_js/Main.js
@@ -13,7 +13,7 @@
 	
 		test_punchAttackInit();
 		
-		testTimer = setTimeout(test_punchAttackApply, 2 * 1000, 2);
+		testTimer = setTimeout(test_punchAttackApply, 2 * 1000, 1, 10);
 	}
 	
 	function test_punchAttackInit()
@@ -23,13 +23,13 @@
 		$(".battleNav-superPunch").html("");
 	}
 	
-	function test_punchAttackApply(playerNum)
+	function test_punchAttackApply(playerNum, punches)
 	{
 		var _id = "battleNav-player" + playerNum;
 		
 		$("#" + _id).addClass("battleNav-superPunchAttacker");
 		
-		for(var i = 0; i < 10; i++)
+		for(var i = 0; i < punches; i++)
 		{
 			var de = i * 0.2;
 			var st = setTimeout(test_punchAttackAdd, de * 1000, _id, i);
@@ -42,52 +42,36 @@
 		
 		$("#" + div + " .battleNav-superPunch").append(html_superPunch);
 		
-		// $('[data-asset == "superPunch"]').attr("id", "superPunch" + num);
-		
-		// $("#" + div + " #superPunch" + num).removeAttr("data-asset");
-		
-		//$("#" + div + " #superPunch" + num).addClass("battleNav-superPunchAttack");
-		
-		//$("#" + div + " #_PUNCH").attr("id", "superPunch" + num);
-		
-		// $("#" + div + " ._PUNCH").attr("id", "superPunch" + num);
-		
-		// $("#" + div + " #superPunch" + num).removeClass("_PUNCH");
-		
 		$("#" + div + " ._PUNCH").addClass("SUPER-PUNCH" + num);
 		$("#" + div + " ." + "SUPER-PUNCH" + num).removeClass("_PUNCH");
-		
-		//$("#" + div + " #superPunch" + num).addClass("battleNav-superPunchAttack");
 	
 		exitFrame = setTimeout(test_punchAttackFire, 20, $("#" + div + " ." + "SUPER-PUNCH" + num));
 	}
 	
 	function test_punchAttackFire(div)
 	{
-		$(div)[0].addEventListener("webkitTransitionEnd", testPunchAttackFireEnd, false);
-		$(div)[0].addEventListener("transitionend", testPunchAttackFireEnd, false);
+		$(div)[0].addEventListener("webkitTransitionEnd", test_punchAttackFireEnd, false);
+		$(div)[0].addEventListener("transitionend", test_punchAttackFireEnd, false);
 		
 		$(div).addClass("battleNav-superPunchAttack");
 	}
 	
-	function testPunchAttackFireEnd(event)
+	function test_punchAttackFireEnd(event)
 	{
-		trace(event);
+		$(event.target)[0].removeEventListener("webkitTransitionEnd", test_punchAttackFireEnd, false);
+		$(event.target)[0].removeEventListener("transitionend", test_punchAttackFireEnd, false);
+			
+		$(event.target)[0].addEventListener("webkitTransitionEnd", test_punchAttackComplete, false);
+		$(event.target)[0].addEventListener("transitionend", test_punchAttackComplete, false);
 		
-		$(event.target)[0].removeEventListener("webkitTransitionEnd", testPunchAttackFireEnd, false);
-		$(event.target)[0].removeEventListener("transitionend", testPunchAttackFireEnd, false);
-		
-		$(event.target)[0].addEventListener("webkitTransitionEnd", testPunchAttackComplete, false);
-		$(event.target)[0].addEventListener("transitionend", testPunchAttackComplete, false);
-	
 		$(event.target).removeClass("battleNav-superPunchAttack").addClass("battleNav-superPunchReturn");
 	}
 	
-	function testPunchAttackComplete(event)
+	function test_punchAttackComplete(event)
 	{
-		$(event.target)[0].removeEventListener("webkitTransitionEnd", testPunchAttackComplete, false);
-		$(event.target)[0].removeEventListener("transitionend", testPunchAttackComplete, false);
+		$(event.target)[0].removeEventListener("webkitTransitionEnd", test_punchAttackComplete, false);
+		$(event.target)[0].removeEventListener("transitionend", test_punchAttackComplete, false);
 		
-		$(event.target).css("opacity", "0");
+		$(event.target).remove();
 	}
 	

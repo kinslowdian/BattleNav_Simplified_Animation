763aec5cfaecb6accde51ff9846dfe6b078eff60
Simon Kinslow <simonkinslow@gmail.com>
2014-07-02 14:12:56 +0100

    Extras
    Graphics update showing colours of claws. Animation added on eyes for Attacker and Victim

--- _assets/_js/Main.js
+++ _assets/_js/Main.js
@@ -7,17 +7,28 @@
 	
 	var testTimer;
 	
+	var BATTLE_NAV;
+	
 	function init()
 	{
 		trace("init();");	
-	
+		
+		
+		BATTLE_NAV = {};
+		
+		BATTLE_NAV.animation = {};
+		
 		test_punchAttackInit();
 		
-		testTimer = setTimeout(test_punchAttackApply, 2 * 1000, 1, 10);
+		testTimer = setTimeout(test_punchAttackApply, 2 * 1000);
 	}
 	
 	function test_punchAttackInit()
 	{
+		BATTLE_NAV.animation.punch_id_attack = "battleNav-player1";
+		BATTLE_NAV.animation.punch_id_victim = "battleNav-player2";
+		BATTLE_NAV.animation.punchTotal = 10;
+		
 		html_superPunch = $(".battleNav-superPunch").html();
 		
 		$(".battleNav-superPunch").html("");
@@ -25,27 +36,32 @@
 	
 	function test_punchAttackApply(playerNum, punches)
 	{
-		var _id = "battleNav-player" + playerNum;
+		$("#" + BATTLE_NAV.animation.punch_id_attack).addClass("battleNav-superPunchAttacker");
 		
-		$("#" + _id).addClass("battleNav-superPunchAttacker");
+		$("#" + BATTLE_NAV.animation.punch_id_attack + " .battleNav-player-eyes").addClass("battleNav-player-eyes-lookF");
 		
-		for(var i = 0; i < punches; i++)
+		for(var i = 0; i < BATTLE_NAV.animation.punchTotal; i++)
 		{
 			var de = i * 0.2;
-			var st = setTimeout(test_punchAttackAdd, de * 1000, _id, i);
+			var st = setTimeout(test_punchAttackAdd, de * 1000, i);
 		}
 	}
 	
-	function test_punchAttackAdd(div, num)
+	function test_punchAttackAdd(num)
 	{
 		var exitFrame;
 		
-		$("#" + div + " .battleNav-superPunch").append(html_superPunch);
+		if(num === 0)
+		{
+			$("#" + BATTLE_NAV.animation.punch_id_victim + " .battleNav-player-eyesSprite").removeClass("battleNav-player-eyesSprite-normal").addClass("battleNav-player-eyesSprite-sad");
+		}
 		
-		$("#" + div + " ._PUNCH").addClass("SUPER-PUNCH" + num);
-		$("#" + div + " ." + "SUPER-PUNCH" + num).removeClass("_PUNCH");
+		$("#" + BATTLE_NAV.animation.punch_id_attack + " .battleNav-superPunch").append(html_superPunch);
+		
+		$("#" + BATTLE_NAV.animation.punch_id_attack + " ._PUNCH").addClass("SUPER-PUNCH" + num);
+		$("#" + BATTLE_NAV.animation.punch_id_attack + " ." + "SUPER-PUNCH" + num).removeClass("_PUNCH");
 	
-		exitFrame = setTimeout(test_punchAttackFire, 20, $("#" + div + " ." + "SUPER-PUNCH" + num));
+		exitFrame = setTimeout(test_punchAttackFire, 20, $("#" + BATTLE_NAV.animation.punch_id_attack + " ." + "SUPER-PUNCH" + num));
 	}
 	
 	function test_punchAttackFire(div)
@@ -73,5 +89,11 @@
 		$(event.target)[0].removeEventListener("transitionend", test_punchAttackComplete, false);
 		
 		$(event.target).remove();
+		
+		if($("#" + BATTLE_NAV.animation.punch_id_attack + " .battleNav-superPunch div").length === 0)
+		{
+			$("#" + BATTLE_NAV.animation.punch_id_attack + " .battleNav-player-eyes").removeClass("battleNav-player-eyes-lookF");
+			$("#" + BATTLE_NAV.animation.punch_id_victim + " .battleNav-player-eyesSprite").removeClass("battleNav-player-eyesSprite-sad").addClass("battleNav-player-eyesSprite-normal");
+		}
 	}
 	
